780e2612e297aa2dbbcb01b34b0a876ce5c2d96b
Kacper Cienkosz <kacper@air-kacper.home>
2023-01-15 14:42:10 +0100

    Setup docker and postgres

--- Backend/bydlac_backend/settings.py
+++ Backend/bydlac_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&-i*6$!vs!bmq3h=ac8b*sy5v)t=xc)d(*696_qcd&p!*1(fl(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.118.99.23', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '172.19.0.2']
 
 
 # Application definition
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "bydlac_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bydlacdb',
+        'USER': 'bydlacdb_user',
+        'PASSWORD': 'bydlac_pswd$31hiald',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
