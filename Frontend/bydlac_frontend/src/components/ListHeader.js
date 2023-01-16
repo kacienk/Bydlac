5dbf37f7e9553e548829bda1b15d116ef892aa8f
Miłosz Dubiel <92731319+dubielel@users.noreply.github.com>
2023-01-16 23:45:26 +0100

    Update ListHeader.js

--- Frontend/bydlac_frontend/src/components/ListHeader.js
+++ Frontend/bydlac_frontend/src/components/ListHeader.js
@@ -4,6 +4,11 @@ import {useState} from "react";
 
 import "./ListHeader.css";
 
+/**
+ * Custom Component which represents choice of lists to display (see: {@link GroupList} and {@link EventList})
+ * @param whichToShow variable representing which list to show depending on Parent Component
+ * @returns {JSX.Element} HTML element - button with functionality to switch between Group List and Event List
+ */
 const ListHeader = ({whichToShow}) => {
     const [whichListToShow, setWhichListToShow] = useState(whichToShow)
 
